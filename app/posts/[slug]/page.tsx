991c36248df57e8146f3e6640636cbeac375c1a5
Albin Touma <albin.touma@complyadvantage.com>
2023-09-06 11:23:10 +0300

    Added styling to blog

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -2,6 +2,9 @@ import fs from 'fs';
 import Markdown from 'markdown-to-jsx'
 import matter from 'gray-matter'
 import getPostMetaData from '@/src/components/Blog/getPostMetadata';
+import { Image } from 'next/dist/client/image-component';
+import NavBar from '@/src/components/Shared/NavBar';
+import { FaArrowLeft } from 'react-icons/fa'
 
 const getPostContent = (slug: string) => {
     const folder = 'posts/'
@@ -22,10 +25,24 @@ const BlogPage = (props: any) => {
     const slug = props.params.slug;
     const post = getPostContent(slug)
     return (
-        <>  <h1>{post.data.title}</h1>
-            <Markdown>{post.content}</Markdown>
+        <>
+            <header className='mb-[100px] mt-11 mr-11 flex justify-end'>
+
+                <a href='/'>
+                    <button className='flex flex-row gap-2 items-center'>
+                        <FaArrowLeft />
+                        Go home
+                    </button>
+                </a>
+            </header>
+            <section className='max-w-[768px] mx-auto'>
+                <h1 className='text-5xl mb-11 text-emerald-900'>{post.data.title}</h1>
+                <Image src={post.data.image} width={768} height={500} alt='blog cover' />
+                <Markdown className='markdown prose-h1:text-4xl prose-p:text-2xl prose-p:text-gray-600 prose-h1:text-emerald-900'>{post.content}</Markdown>
+            </section>
         </>
     )
+
 
 
 
