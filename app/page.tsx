d091499bf195cbbeed4037936cc51d7a7f483ce8
Albin Touma <albin.touma@complyadvantage.com>
2023-09-27 17:51:59 +0300

    Added key to preview

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,18 @@ import Home from '../src/components/Home/index.js';
 import BlogPreview from "../src/components/Blog/frontcard";
 import Calendly from '../src/components/Home/contactMe.jsx';
 import FooterSection from '../src/components/Home/footer.jsx';
+import getPostMetaData from "../src/components/Blog/getPostMetadata";
 
 export default function HomePage() {
+  const postMetaData = getPostMetaData();
+  const postPreviews = postMetaData.map((post) => (
+    <BlogPreview key={post.slug} {...post} />
+  ));
+
   return (
     <>
       <Home />
-      <BlogPreview />
+      <div>{postPreviews}</div>
       <Calendly />
       <FooterSection />
     </>
